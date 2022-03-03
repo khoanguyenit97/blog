6212f14f44d0bfe8b191880ece941cf4f0fed6cf
khoanda10 <khoanda10@gmail.com>
2022-03-03 17:15:52 +0700

    finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,12 +59,12 @@ class ArticlesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
