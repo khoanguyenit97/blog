8c96d0ec278744d98d43ccb29ef7d13bbb9be569
khoanda10 <khoanda10@gmail.com>
2022-03-09 10:34:46 +0700

    Add email for user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_141235) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_033025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,9 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_141235) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "email"
   end
 
 end
