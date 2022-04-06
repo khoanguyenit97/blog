c389c80b1926078f3af1e1fb56fbe73335193848
khoanda10 <khoanda10@gmail.com>
2022-04-06 19:17:32 +0700

    first

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   resources :articles
   # get 'page/home'
-  root 'page#home'
+  root 'articles#index'
 
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
